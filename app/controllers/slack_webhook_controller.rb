ec99469fd266ebf5ba7d94958c5d498104c9c4e1
Jeroen Visser <jeroenvisser101@gmail.com>
2015-02-27 18:31:36 +0100

    Simplified the controller

--- app/controllers/slack_webhook_controller.rb
+++ app/controllers/slack_webhook_controller.rb
@@ -1,13 +1,27 @@
 class SlackWebhookController < ApplicationController
+  before_action :check_token
+
   def incoming_message
+    message = Message.create(message_params)
+    if message
+      render status: :ok
+    else
+      logger.fatal 'Could not save message to database.'
+      render status: :internal_server_error
+    end
+  end
+
+  def check_token
     unless params[:token] == Rails.application.secrets.slack_secret_token
       logger.fatal 'Slack secret token does not match configured token.'
       render status: :forbidden
     end
+  end
 
-    message_data = {
-      channel_name: params[:channel_name],
+  def message_params
+    {
       username: params[:user_name],
+      channel: params[:channel_name],
       message: params[:text]
     }
   end
