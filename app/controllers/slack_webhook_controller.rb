ffd4bf893499884e0540098e40d681653850ea43
Jeroen Visser <jeroenvisser101@gmail.com>
2015-02-27 19:19:39 +0100

    Added text to render

--- app/controllers/slack_webhook_controller.rb
+++ app/controllers/slack_webhook_controller.rb
@@ -5,17 +5,17 @@ class SlackWebhookController < ApplicationController
   def incoming_message
     message = Message.create(message_params)
     if message
-      render status: :ok
+      render text: '', status: :ok
     else
       logger.fatal 'Could not save message to database.'
-      render status: :internal_server_error
+      render text: 'Could not save message.', status: :internal_server_error
     end
   end
 
   def check_token
     unless params[:token] == Rails.application.secrets.slack_secret_token
       logger.fatal 'Slack secret token does not match configured token.'
-      render status: :forbidden
+      render text: 'Forbidden', status: :forbidden
     end
   end
 
