703ece6656f74a3d0ef893eb9f6a7579fb5a9f93
Jeroen Visser <jeroenvisser101@gmail.com>
2015-03-01 18:33:21 +0100

    Added IP logging (just to see if IP is set)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   whitelist = lambda { |req|
     whitelisted_ips = Redroom::Application.config.whitelisted_ips
     whitelisted_ips.include? req.remote_addr
+    Rails.logger.warn('IP: ' << req.remote_addr)
   }
 
   scope constraints: whitelist do
